fe9f281cb76917b151acd950b78d646af26f72f4
qifaaltaf <qifaaltaf1@gmail.com>
2025-04-07 20:11:34 +0530

    Shape area and perimeter calculations

---
+++ Assignments.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+//area and perimeter of SQUARE
+//float side, area, peri; 
+//printf("Enter side of Square:\n");
+//scanf("%f",&side);
+//area=side*side;
+//peri=4*side;
+//printf("AREA of the given Square is : %f\n Perimeter of the given Square is : %f\n",area,peri);
+
+//area and perimeter of RECTANGLE
+//float len, brdth, area, peri; 
+//printf("Enter length of Rectangle:\n");
+//scanf("%f",&len);
+//area=len*brdth;
+//peri=2*(len+brdth);
+//printf("AREA of the given Rectangle is : %f\n Perimeter of the given Reactangle is : %f\n",area,peri);
+
+//area and perimeter of CIRCLE
+//float radius;
+//printf("Enter radius of Circle:\n");
+//scanf("%f",&radius);
+//float area, peri;
+//area=3.14*radius*radius;
+//peri=2*3.14*radius;
+//printf("AREA of the given circle is : %f\n Perimeter of the given circle is : %f\n",area,peri);
+
+//area and perimeter of TRIANGLE
+//float side1, side3, side2, base, height, area, peri; 
+//printf("Enter first side of Triangle :\n");
+//scanf("%f",&side1);
+//printf("Enter second side of Triangle:\n");
+//scanf("%f",&side2);
+//printf("Enter third side of Triangle:\n");
+//scanf("%f",&side3);
+//printf("Enter height of Triangle:\n");
+//scanf("%f",&height);
+//printf("Enter base of Triangle:\n");
+//scanf("%f",&base);
+//area=(base*height)/2;S
+//peri=side1+side2+side3;
+//printf("Area of the given triangle is : %f\n Perimeter of the given triangle is : %f\n",area,peri);
+
+//area and perimeter of Parallelogram
+//float base, height, area , peri, side1, side2;
+//printf("Enter Base of Parallelogram:\n");
+//scanf("%f",&base);
+//printf("Enter Height of Parallelogram:\n");
+//scanf("%f",&height);
+//area=base*height;
+//printf("Enter First side of Parallelogram:\n");
+//scanf("%f",&side1);
+//printf("Enter Second side of Parallelogram:\n");
+//scanf("%f",&side2);
+//peri=2*(side1+side2);
+//printf("AREA of the given Parallelogram is : %f\n Perimeter of given Parallelogram is : %f",area,peri);
+
+//area and perimeter of RHOMBUS
+//float side, dia1, dia2, area, peri; 
+//printf("Enter SIDE of Rhombus:\n");
+//scanf("%f",&side);
+//printf("Enter First diagonal of Rhombus:\n");
+//scanf("%f",&dia1);
+//printf("Enter Second diagonal of Rhombus:\n");
+//scanf("%f",&dia2);
+//area=(dia1*dia2)/2;
+//peri=4*side;
+//printf("AREA of the given Rhombus is : %f\n Perimeter of given Rhombus is : %f",area,peri);
+
+//area and perimeter of TRAPEZIUM  
+//float side1, side3, side2, side4, height, area, peri; 
+//printf("Enter first side of Trapezium :\n");
+//scanf("%f",&side1);
+//printf("Enter second side of Trapezium :\n);
+//scanf("%f",&side2);
+//printf("Enter third side of Trapezium:\n");
+//scanf("%f",&side3);
+//printf("Enter forth side of Trapezium:\n");
+//scanf("%f",&side4);
+//printf("Enter Second height of Rhombus:\n");
+//scanf("%f",&height);
+//area=(side1+side2)*height/2;
+//peri=side1+side3+side3+side4;
+//printf("AREA of the given Rhombus is : %f\n Perimeter of given Rhombus is : %f",area,peri);
+
+//area and perimeter of a regular PENTAGON
+//float side, apothem,area, peri;
+//printf("Enter side of pentagon: ");
+//scanf("%f",&side);
+//printf("Enter apothem of pentagon: ");
+//scanf("%f",&apothem);
+//peri=5*side;
+//area=(peri*apothem)/2;
+//printf("AREA of the given Pentagon is : %f\n Perimeter of given pentagon is : %f",area,peri);
+
+//area and perimeter of a regular HEXAGON
+//float side, area, peri;
+//printf("Enter side of pentagon: ");
+//scanf("%f",&side);
+//peri=6*side;
+//area=(3*sqrt(3)*side*side)/2;
+//printf("AREA of the given Hexagon is : %f\n Perimeter of given hexagon is : %f",area,peri);
+
+
+}
+
